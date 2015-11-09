77bfc1bb35a9888a5ee2e617e73d7bcac210f01e
GloriaPG <ing.gloriapalmagonzalez@gmail.com>
2015-11-09 02:09:43 -0600

    Settings pagination

--- ubin_rest/ubin_rest/settings.py
+++ ubin_rest/ubin_rest/settings.py
@@ -60,7 +60,9 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
     ),
-
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100,
 }
 
 FILE_UPLOAD_HANDLERS = (
