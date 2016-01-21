d3bdfbf6b0cb8f7e0bd1d93ca2f5a71bd92176b3
GloriaPG <ing.gloriapalmagonzalez@gmail.com>
2016-01-21 02:14:03 -0500

    server-commit

--- ubin_rest/apprest/migrations/0001_initial.py
+++ ubin_rest/apprest/migrations/0001_initial.py
@@ -83,6 +83,9 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.TextField(max_length=100)),
+                ('symbol', models.TextField(max_length=2)),
+                ('code', models.TextField(max_length=3)),
+                ('value', models.DecimalField(max_digits=2, decimal_places=2)),
                 ('status', models.BooleanField(default=True)),
             ],
         ),
@@ -96,6 +99,17 @@ class Migration(migrations.Migration):
                 ('phone', models.TextField(max_length=0)),
                 ('email', models.EmailField(max_length=100)),
                 ('status', models.BooleanField(default=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Devices_User_Register',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('device_os', models.TextField(max_length=30)),
+                ('device_token', models.TextField(max_length=300, unique=True, null=True, blank=True)),
+                ('device_register_date', models.DateField(auto_now_add=True)),
+                ('device_status', models.BooleanField(default=True)),
+                ('device_user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
         ),
         migrations.CreateModel(
@@ -368,7 +382,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='favorites_providers',
             name='provider',
-            field=models.ForeignKey(to='apprest.Providers'),
+            field=models.ForeignKey(related_name='favorite', to='apprest.Providers'),
         ),
         migrations.AddField(
             model_name='favorites_providers',
@@ -378,7 +392,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='favorites',
             name='publication',
-            field=models.ForeignKey(to='apprest.Publications'),
+            field=models.ForeignKey(related_name='favorite', to='apprest.Publications'),
         ),
         migrations.AddField(
             model_name='favorites',
@@ -423,7 +437,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='classification_providers',
             name='provider',
-            field=models.ForeignKey(to='apprest.Providers'),
+            field=models.ForeignKey(related_name='score', to='apprest.Providers'),
         ),
         migrations.AddField(
             model_name='classification_providers',
