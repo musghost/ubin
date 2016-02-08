7991b3b6f9070e5183ffce3ff97909b9a76b97a6
GloriaPG <ing.gloriapalmagonzalez@gmail.com>
2016-02-08 02:39:49 -0500

    merge migratons

---
+++ ubin_rest/apprest/migrations/0034_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('apprest', '0033_auto_20160208_0713'),
+        ('apprest', '0033_auto_20160208_0511'),
+    ]
+
+    operations = [
+    ]
