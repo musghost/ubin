4b7df8f0756b1b4abdc53c1c037bb5ec35ef954a
GloriaPG <ing.gloriapalmagonzalez@gmail.com>
2016-02-08 01:17:16 -0500

    migration model favorite

---
+++ ubin_rest/apprest/migrations/0033_auto_20160208_0511.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('apprest', '0032_auto_20160208_0421'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='favorites',
+            name='user',
+            field=models.ForeignKey(related_name='favorite', to=settings.AUTH_USER_MODEL),
+        ),
+    ]
