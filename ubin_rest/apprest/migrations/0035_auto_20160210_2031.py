5348652271cb97a01507d97bf4ef9b49842c4a23
GloriaPG <ing.gloriapalmagonzalez@gmail.com>
2016-02-10 14:32:11 -0600

    Migrations events, report, user location

---
+++ ubin_rest/apprest/migrations/0035_auto_20160210_2031.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('apprest', '0034_merge'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='reports',
+            name='date',
+            field=models.DateField(),
+        ),
+        migrations.AlterField(
+            model_name='tasks',
+            name='date',
+            field=models.DateField(),
+        ),
+        migrations.AlterField(
+            model_name='user_location',
+            name='date',
+            field=models.DateTimeField(auto_now_add=True),
+        ),
+        migrations.AlterUniqueTogether(
+            name='favorites',
+            unique_together=set([('publication', 'user')]),
+        ),
+    ]
