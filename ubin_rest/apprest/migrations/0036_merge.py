7475ab86165f856ae42d389a52bfb2a78233fcee
GloriaPG <ing.gloriapalmagonzalez@gmail.com>
2016-02-10 15:38:24 -0500

    Add merge migrations events

---
+++ ubin_rest/apprest/migrations/0036_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('apprest', '0035_auto_20160210_0818'),
+        ('apprest', '0035_auto_20160210_2031'),
+    ]
+
+    operations = [
+    ]
